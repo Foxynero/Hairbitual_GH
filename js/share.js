27af821e5957724a72e854086a3761578f52d844
FOXYNERO <113718153+Foxynero@users.noreply.github.com>
2023-09-17 00:34:13 +0000

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 document.getElementById('shareImage').addEventListener('click', function() {
     var message = encodeURIComponent('Check out this awesome image!');
-    var url = 'whatsapp://send?text=' + message;
+    var url = 'https://wa.me/+233597421076' + message;
     window.location.href = url;
 });
