ec30b793ff1e0310dbe87a9df69c1ed3e2fe64e3
FOXYNERO <113718153+Foxynero@users.noreply.github.com>
2023-09-17 01:42:20 +0000

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 function sendWhatsAppMessage(phoneNumber, imageName) {
-    var imageURL = 'https://github.com/Foxynero/Vincee/blob/main/images/gallery/' + imageName; // Replace with the actual URL for the image.
+    var imageURL = 'https://github.com/Foxynero/Vincee/blob/main/images/gallery/1.jpeg' + imageName; // Replace with the actual URL for the image.
     var message = 'Check out this image: ' + imageURL;
     var whatsappLink = 'https://api.whatsapp.com/send?phone=' + encodeURIComponent(phoneNumber) + '&text=' + encodeURIComponent(message);
 
