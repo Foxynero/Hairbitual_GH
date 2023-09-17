3c1960710df24d0be3ce7e1e4e85389cdd64fda8
FOXYNERO <113718153+Foxynero@users.noreply.github.com>
2023-09-17 00:45:52 +0000

    Update share.js

--- js/share.js
+++ js/share.js
@@ -1,5 +1,5 @@
 document.getElementById('shareImage').addEventListener('click', function() {
-    var message = encodeURIComponent('Check out this awesome image!');
-    var url = 'whatsapp://send?text=' + message;
+    var message = encodeURIComponent('Check out this awesome image: https://raw.githubusercontent.com/Foxynero/Vincee/main/images/gallery/1.jpeg');
+    var url = 'https://wa.me/+233597421076?text=' + message;
     window.location.href = url;
 });
